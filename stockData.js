fc9abba28a599f462c72990165063462406db5e3
Vladislav <paklosha@gmail.com>
2021-07-11 14:05:16 +0300

    Update stockData.js

--- stockData.js
+++ stockData.js
@@ -1,31 +1,31 @@
 module.exports = {
     data : [] =[
         {
-            name: "Dow Jones",
+            name: "GAZPROM",
             value: 24331.32,
             change: 455.43,
             percentage: 1.91
         },
         {
-            name: "NASDAQ",
+            name: "SBERBANK",
             value: 9121.32,
             change: 141.66,
             percentage: 1.58
         },
         {
-            name: "Xetra DAX",
+            name: "NOVATEK",
             value: 10904.48,
             change: 145.21,
             percentage: 1.35
         },
         {
-            name: "Paris CAC 40",
+            name: "SURGUTNEFTEGAS",
             value: 4549.64,
             change: 48.20,
             percentage: 1.07
         },
         {
-            name: "Nikkei 225",
+            name: "ROSNEFT",
             value: 20179.09,
             change: 504.32,
             percentage: 2.56
